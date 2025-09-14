34456a3feccaef0dd0858257ea29c9175f1e76f8
user <hag.hispin12@gmail.com>
2025-09-14 15:15:21 +0300

    version4

--- app/src/main/java/com/example/mathprojectdaniel/Exercise.java
+++ app/src/main/java/com/example/mathprojectdaniel/Exercise.java
@@ -9,10 +9,12 @@ public class Exercise {
     private int score;
     private int answer;
     private Toast toaster;
+    private int currentDiff;
     private ExerciseListener exerciseListener;
     public Exercise(MainActivity context, ExerciseListener EL){
         score = 0;
         answer = 0;
+        currentDiff = 0;
         toaster = new Toast(context);
         toaster.setDuration(Toast.LENGTH_SHORT);
         exerciseListener = EL;
@@ -25,23 +27,26 @@ public class Exercise {
         int mana1 = randInt(9);
         int mana2 = randInt(9);
         answer = mana1 * mana2;
+        currentDiff = 1;
         exerciseListener.setManas(mana1,mana2);
     }
     public void normal() {
         int mana1 = randInt(9);
         int mana2 = randInt(20);
         answer = mana1 * mana2;
+        currentDiff = 2;
         exerciseListener.setManas(mana1,mana2);
     }
     public void hard() {
         int mana1 = randInt(9);
         int mana2 = randInt(99);
         answer = mana1 * mana2;
+        currentDiff = 4;
         exerciseListener.setManas(mana1,mana2);
     }
     public void check(int userAnswer){
         if(answer == userAnswer){
-            score++;
+            score += 5 * currentDiff;
             exerciseListener.correct(score);
         }
         else {
